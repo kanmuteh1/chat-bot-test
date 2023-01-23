4e6db7e2913e5587407b18ae1f37ea5e1264f156
kanmuteh1 <janmekgayejt@gmail.com>
2023-01-23 18:45:23 +0000

    refix file structure

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ import express from 'express';
 const app = express()
 const port = 3000
 
+// EAADPC6KVsc0BAHcJvgdjJoKqZByDJZCP89EESpU1CrZCctHQOr9rsfBXLhIyoXytci5HZAwQrAtCOiwcTOemNYnwFs21mJlhsCindwYnOTT0xNx07DZBojGtVxNYqJ6Nba2mLj88XB8Eb75qVaYSWZA9c32xh9HORc2LX5ZBuIx2PXaki23ENuB
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
@@ -14,6 +15,4 @@ app.get('/webhook/',(req, res) =>{
   res.send("wrong token")
 }) 
 
-export function start(){
-    app.listen(port, () => console.log(`Example app listening on port ${port}`))
-}
+app.listen(port, () => console.log(`Example app listening on port ${port}`))
--- index.js
+++
@@ -1,3 +0,0 @@
-import { start } from "./src/app.js";
-
-start();
