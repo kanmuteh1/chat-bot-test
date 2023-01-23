fb340f750d5300265a726b4460b713f75883ad6d
kanmuteh1 <janmekgayejt@gmail.com>
2023-01-23 00:58:20 +0000

    chatbot test

--- src/app.js
+++ src/app.js
@@ -2,9 +2,17 @@ import express from 'express';
 const app = express()
 const port = 3000
 
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+
+app.get('/webhook/',(req, res) =>{
+  if (req.query['hub.verify_token'] === "gbolo"){
+    res.send(req.query('hub.challenge'))
+  }
+  res.send("wrong token")
+}) 
 
 export function start(){
     app.listen(port, () => console.log(`Example app listening on port ${port}`))
